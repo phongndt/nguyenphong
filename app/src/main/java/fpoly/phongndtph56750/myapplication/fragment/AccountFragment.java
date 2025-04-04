1c9fd9a8c9fb84f8f077cd0eb0845fda4ba064b5
WIN-9QANVT2PR3F\Administrator <tu1den19@gmail.com>
2025-04-04 12:10:24 +0700

    booking

--- app/src/main/java/fpoly/phongndtph56750/myapplication/fragment/AccountFragment.java
+++ app/src/main/java/fpoly/phongndtph56750/myapplication/fragment/AccountFragment.java
@@ -2,22 +2,50 @@ package fpoly.phongndtph56750.myapplication.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import fpoly.phongndtph56750.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import fpoly.phongndtph56750.myapplication.activity.ChangePasswordActivity;
+import fpoly.phongndtph56750.myapplication.activity.SignInActivity;
+import fpoly.phongndtph56750.myapplication.constant.GlobalFunction;
+import fpoly.phongndtph56750.myapplication.databinding.FragmentAccountBinding;
+import fpoly.phongndtph56750.myapplication.prefs.DataStoreManager;
 
 
 public class AccountFragment extends Fragment {
 
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+    public View onCreateView(@Nullable LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        FragmentAccountBinding fragmentAccountBinding = FragmentAccountBinding.inflate(inflater, container, false);
+
+        fragmentAccountBinding.tvEmail.setText(DataStoreManager.getUser().getEmail());
+
+        fragmentAccountBinding.layoutSignOut.setOnClickListener(l -> onClickSignOut());
+        fragmentAccountBinding.layoutChangePassword.setOnClickListener(l -> onClickChangePassword());
+
+        return fragmentAccountBinding.getRoot();
+    }
+
+    private void onClickChangePassword() {
+        GlobalFunction.startActivity(getActivity(), ChangePasswordActivity.class);
+    }
+
+    private void onClickSignOut(){
+        if (getActivity()== null){
+            return;
+        }
+        FirebaseAuth.getInstance().signOut();
+        DataStoreManager.setUser(null);
+        GlobalFunction.startActivity(getActivity(), SignInActivity.class);
+        getActivity().finishAffinity();
     }
 }
